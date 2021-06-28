8624b0b687c2383eebfd296090e52e85a35f97a8
unknown <giga.giorgadze.11@gmail.com>
2021-06-28 16:06:37 +0400

    fixing a bug

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const temp = document.querySelector('#temp')
 const feelingTemp = document.querySelector('#feelingTemp')
 const humidity = document.querySelector('#humidity')
 const wind = document.querySelector('#wind')
+const error = document.querySelector('#error')
 let citySearched = ''
 async function getCity(){
     try{
         const response = await  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&&units=metric&APPID=c798a96f7f093719f778f3b8bd60f3f6`, {mode: 'cors'})
         const cityData = await response.json()
         console.log(cityData)
-      
+        error.innerText = ''
         forecast.style.display = 'block'
         cityName.innerText = `${cityData.name}`
         temp.innerText = `${cityData.main.temp}°C`
         feelingTemp.innerText = `Feels like: ${cityData.main.feels_like}°C`
         humidity.innerText = `Humidity: ${cityData.main.humidity}%`
         wind.innerText = `Wind: ${cityData.wind.speed} km/h`
+        error.innerText = ''
     }catch (err){
         if(err.feels_like == undefined){
             cityName.innerText = ''
@@ -26,9 +28,9 @@ async function getCity(){
             humidity.innerText = ''
             wind.innerText = ''
             forecast.style.display = 'block'
-            const error = document.createElement('h1')
             error.innerText = `city with that name doesn't exsist in our database try another city or check for type errors`
             forecast.appendChild(error)
+            console.log(error)
         }else{
             console.log(err)
         }
